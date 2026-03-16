c4c3c60e5ac1df6cd426247f9fb484e312a72747
Raiyan Sohel <afnan19262@gmail.com>
2026-03-17 01:10:44 +0600

    customer end point added

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Customer } from './interfaces/customer.interface';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  private customers: Customer[] = [];
+  getAllCustomers(): Customer[] {
+    return this.customers;
+  }
+  addCustomer(createCustomerDto: CreateCustomerDto): Customer {
+    const newCustomer: Customer = {
+      id: Date.now(),
+      ...createCustomerDto,
+    };
+    this.customers.push(newCustomer);
+    return newCustomer;
+  }
+}
